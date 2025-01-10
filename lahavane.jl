50a5e8d978487ea13aca5590c77df6ee98a8bc3a
philein-zso <philippe.elong@fdti-camerquants.com>
2025-01-10 13:45:43 +0100

    Update lahavane.jl

--- lahavane.jl
+++ lahavane.jl
@@ -26,23 +26,39 @@ end
 # change the TimePoint type into
 # something more robust and concize
 # here's a proposal
-begin
-  const TimePoint = Union{<:Real,<:TimeType}
-end
+# begin
+#    const TimePoint = Union{<:Real,<:TimeType}
+# end
 
 
 begin
   function simulate(
-                    m::BlackScholes,
-                    Tuple{A,B},
-                    from::D,
-                    to::D,
-                    dt::Period;
-                    n::Int64=1,
-                    x0::R=1.0,
-                    nsteps::C=0,
-                    rn::Bool=false
-           ) where {A<:Real,B<:Real,C<:Integer,D<:Union{Date,AbstractDateTime}}
-    end
-                    
+                    m::BlackScholes, par::Tuple{A,B},
+                    from::T, to::T, dt::Period;
+                    n::Integer=1, nsteps::Integer=0,
+                    x0::R=1.0, rn::Bool=false,
+                    dcc::DayCounyConventions=ActAct()
+           ) where {A<:Real,B<:Real,T<:TimeType,R<:Real}
+    
+  end
+
+  function simulate(
+                    m::BlackScholes, par::Tuple{A,B},
+                    from::T1, to::T2, dt::T3;
+                    n::Integer=1, nsteps::Integer=1,
+                    x0::R=1.0, rn::Bool=false,
+                    dcc::DayCountConventions=ActAct()
+           ) where {A<:Real,B<:Real,T1<:Real,T2<:Real,T3<:Real,R<:Real}
+    nothing
+  end
+
+  function simulate(
+                    m::BlackScholes, obs::Observables,
+                    from::T, to::T, dt::Union{Period,<:Real};
+                    n::Integer=1, nsteps::Integer=1,
+                    x0::R=1.0, rn::Bool=false,
+                    dcc::DayCountConventions=ActAct()
+           ) where {T<:TimePoint,R<:Real}
+    nothing
+  end
 end
