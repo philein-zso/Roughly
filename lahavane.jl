ebac6aeb1ce144b9e9f9333437203d78ec4793f4
philein-zso <philippe.elong@fdti-camerquants.com>
2025-01-10 17:06:38 +0100

    Update lahavane.jl

--- lahavane.jl
+++ lahavane.jl
@@ -42,30 +42,43 @@ begin
     t  = collect(StepRange(from, dt, to))
     in(to, t) ? nothing : push!(t, to)
     _t = vcat(0.0, diff(dcc, t))
-    _n = max(nsteps, ceil(Int64, /(size(_t,1),max(_t))))
+    _n = max(nsteps, ceil(Int64, /(size(_t,1),max(_t)))) - 1
     wi = WienerProcess(n, sum(_t), nsteps=_n, dimension=1)
     simulate!(wi)
     β  = ifelse(rn, S(m.riskfreerate)(from), par[1]-0.5par[2]^2)
-    (t, x0 * exp.(.+(β * cumsum(_t), σ * wi.paths)))
+    (t, x0 * exp.(.+(β * cumsum(_t), par[2] * wi.paths)))
   end
 
   function simulate(
                     m::BlackScholes, par::Tuple{A,B},
                     from::T1, to::T2, dt::T3;
-                    n::Integer=1, nsteps::Integer=1,
+                    n::Integer=1, nsteps::Integer=0,
                     x0::R=1.0, rn::Bool=false,
                     dcc::DayCountConventions=ActAct()
            ) where {A<:Real,B<:Real,T1<:Real,T2<:Real,T3<:Real,R<:Real}
-    nothing
+    @assert isless(0.0, dt)
+    t  = collect(LinRange(from, to, ceil(Int64, /(to - from, dt))))
+    wi = WienerProcess(n, last(t), nsteps=max(size(t,1)-1, nsteps), dimension=1)
+    simulate!(wi)
+    β  = ifelse(rn, S(m.riskfreerate)(from), par[1]-0.5par[2]^2)
+    (t, x0 * exp.(.+(β * t, par[2] * wi.paths)))
   end
 
   function simulate(
                     m::BlackScholes, obs::Observables,
                     from::T, to::T, dt::Union{Period,<:Real};
-                    n::Integer=1, nsteps::Integer=1,
+                    n::Integer=1, nsteps::Integer=0,
                     x0::R=1.0, rn::Bool=false,
                     dcc::DayCountConventions=ActAct()
            ) where {T<:TimePoint,R<:Real}
-    nothing
+    for k in m.required
+      @assert hasparam(obs, k)
+    end
+    simulate(
+             m, (getparams(obs, "μ"), getparams(obs, "σ")),
+             from, to, dt, n=n, nsteps=nsteps, x0=x0,
+             rn=rn, dcc=dcc
+    )
   end
+  
 end
