0f68d8172686e97aa2072275f43766421d77dc06
philein-zso <philippe.elong@fdti-camerquants.com>
2025-01-22 17:38:38 +0100

    Update somerfctrs.jl

--- somerfctrs.jl
+++ somerfctrs.jl
@@ -1,9 +1,19 @@
 #> bonds_pricers, under specific models
 
 #> refactoring of OU simulation
+#
+# Maybe should be defining the following facility
+#
+function op(ξ::R) where R<:Real
+  _f(x::A,y::B) where {A<:Real,B<:Real} = muladd(x,ξ,y)
+end
+
+
 begin
   function simulate(ou::OrnsteinUhlenbeckProcess; x0::R=one(Real)) where R<:Real
-    nothing
+    x = rand(Normal(0.0, 1.0), getfield(ou, :kinda))
+    x .= .*(ou.σ * sqrt.(./(1 .- exp.(-2θ * τ), 2θ)), x)
+    
   end
 
 end
