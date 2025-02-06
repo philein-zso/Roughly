095dd0d195d17332785372874b8cff661d0ef0c7
philein-zso <philippe.elong@fdti-camerquants.com>
2025-02-06 13:25:48 +0100

    Update mv_artemis.jl

--- mv_artemis.jl
+++ mv_artemis.jl
@@ -1,1 +1,23 @@
 # Artemis
+let bunch="Artemis"
+  bry_pit = joinpath(homedir(), bunch)
+  hjo_pit = joinpath(homedir(), bunch, "Dailies")
+  tickers = readdir(brypit, join=false)
+  filter!(endswith(".csv"), tickers)
+  map!(s -> replace(s,".csv" => ""), tickers, tickers)
+  printstyled("Summary\nTickers:$(length(tickers))\nContinue?",color=11)
+  readuntil(stdin,"\n")
+  stocks = Vector{Stock}(undef,length(tickers))
+  for i in 1:length(tickers)
+    try
+      stocks[i] = Stock(tickers[i])
+      setparams!(stocks[i], UnitedStatesEx())
+    catch er
+      printstyled(er,color=4)
+      printstyled(tickers[i],Char(0x2718))
+    end
+  end
+  printstyled("Summary[Observables]\nTickers:$(length(stocks))\nContinue?",color=11)
+  readuntil(stdin,"\n")
+  
+end
